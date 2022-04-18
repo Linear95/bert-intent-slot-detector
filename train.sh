44266efb51ce958615914ebdcade7e93df9ff016
Pengyu Cheng <30513356+Linear95@users.noreply.github.com>
2022-04-18 12:56:02 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -4,9 +4,9 @@ python train.py\
        --cuda_devices 0\
        --tokenizer_path "bert-base-chinese"\
        --model_path "bert-base-chinese"\
-       --train_data_path "/home/pengyu/workspace/bert-intent-slot-detector-public/data/SMP2019/train.json"\
+       --train_data_path "data/SMP2019/train.json"\
        --intent_label_path "data/SMP2019/intent_labels.txt"\
        --slot_label_path "data/SMP2019/slot_labels.txt"\
-       --save_dir "/home/pengyu/workspace/saved_models/${MODEL_NAME}"\
-       --batch_size 10\
+       --save_dir "../saved_models/${MODEL_NAME}"\
+       --batch_size 64\
        --train_epochs 5
